3ca07b231f5dfa9580d4d80761993ca76949d79c
gustaasb <118456961+gustaasb@users.noreply.github.com>
2022-12-15 12:08:27 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -20,11 +20,54 @@ const flappyBird = {
                 flappyBird.largura, flappyBird.altura,
             );
         }
-    }
+}
+
+const chao = {
+    spriteX: 0,
+    spriteY: 611,
+    largura: 225,
+    altura: 112,
+    x: 0,
+    y: 370,
+        desenha(){
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                321, chao.altura,
+            );
+        }
+}
+    
+const plano = {
+    spriteX: 390,
+    spriteY: 4,
+    largura: 276,
+    altura: 199,
+    x: 0,
+    y: 280,
+        desenha(){
+            contexto.drawImage(
+                sprites,
+                plano.spriteX, plano.spriteY,
+                plano.largura, plano.altura,
+                plano.x, plano.y,
+                321, plano.altura,
+            );
+        }
+}
+
+contexto.fillStyle= '#70c5ce';
+contexto.fillRect(0,0, canvas.width, canvas.height)
+    
+    
 
 function loop(){
 
     flappyBird.desenha();
+    plano.desenha();
+    chao.desenha();
     requestAnimationFrame(loop);
 }
 loop();
